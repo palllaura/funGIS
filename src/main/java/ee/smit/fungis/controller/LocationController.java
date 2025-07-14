74dddaa53ae513579d877514d5e0aba7f49f2b13
palllaura <palllaura.tln@gmail.com>
2025-07-14 09:13:20 +0300

    Controller method and tests to get all locations

--- src/main/java/ee/smit/fungis/controller/LocationController.java
+++ src/main/java/ee/smit/fungis/controller/LocationController.java
@@ -1,9 +1,18 @@
 package ee.smit.fungis.controller;
 
+import ee.smit.fungis.entity.Location;
 import ee.smit.fungis.service.LocationService;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
+@RequestMapping("/api")
+
 public class LocationController {
     private final LocationService service;
 
@@ -13,5 +22,40 @@ public class LocationController {
      */
     public LocationController(LocationService service) {
         this.service = service;
+    }
+
+    /**
+     * Get all locations from the database.
+     * @return map of locations.
+     */
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, Object> getAllLocations() {
+        List<Location> locations = service.getAllLocations();
+
+        List<Map<String, Object>> features = locations.stream().map(location -> {
+            Map<String, Object> geometry = Map.of(
+                    "type", "Point",
+                    "coordinates", List.of(
+                            location.getCoordinates().getX(),
+                            location.getCoordinates().getY()
+                    )
+            );
+
+            Map<String, Object> properties = Map.of(
+                    "id", location.getId(),
+                    "description", location.getDescription()
+            );
+
+            return Map.of(
+                    "type", "Feature",
+                    "geometry", geometry,
+                    "properties", properties
+            );
+        }).toList();
+
+        return Map.of(
+                "type", "FeatureCollection",
+                "features", features
+        );
     }
 }
--- src/test/java/ee/smit/fungis/LocationControllerTests.java
+++ src/test/java/ee/smit/fungis/LocationControllerTests.java
@@ -1,11 +1,24 @@
 package ee.smit.fungis;
 
 import ee.smit.fungis.controller.LocationController;
+import ee.smit.fungis.entity.Location;
 import ee.smit.fungis.service.LocationService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.geom.PrecisionModel;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+import java.util.Map;
+
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class LocationControllerTests {
@@ -18,4 +31,52 @@ class LocationControllerTests {
         service = mock(LocationService.class);
         controller = new LocationController(service);
     }
+
+    /**
+     * Helper method to create a location.
+     * @return location object.
+     */
+    private Location createTestLocation() {
+        Location location = new Location();
+        location.setId(555L);
+
+        GeometryFactory factory = new GeometryFactory(new PrecisionModel(), 4326);
+        Point point = factory.createPoint(new Coordinate(1111, 2222));
+        location.setCoordinates(point);
+
+        location.setDescription("Exactly 3 chanterelles");
+
+        return location;
+    }
+
+    @Test
+    void testGetAllLocationsTriggersCorrectMethodInService() {
+        controller.getAllLocations();
+        verify(service, times(1)).getAllLocations();
+    }
+
+    @Test
+    void testGetAllLocationsCorrectOutput() {
+        Location location = createTestLocation();
+        when(service.getAllLocations()).thenReturn(List.of(location));
+
+        Map<String, Object> result = controller.getAllLocations();
+
+        Assertions.assertEquals("FeatureCollection", result.get("type"));
+
+        List<Map<String, Object>> features = (List<Map<String, Object>>) result.get("features");
+        Assertions.assertEquals(1, features.size());
+
+        Map<String, Object> feature = features.getFirst();
+        Assertions.assertEquals("Feature", feature.get("type"));
+
+        Map<String, Object> geometry = (Map<String, Object>) feature.get("geometry");
+        Assertions.assertEquals("Point", geometry.get("type"));
+        Assertions.assertEquals(List.of(1111.0, 2222.0), geometry.get("coordinates"));
+
+        Map<String, Object> properties = (Map<String, Object>) feature.get("properties");
+        Assertions.assertEquals(555L, properties.get("id"));
+        Assertions.assertEquals("Exactly 3 chanterelles", properties.get("description"));
+    }
+
 }
